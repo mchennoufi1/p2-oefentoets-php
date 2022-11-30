b34b540750e256107fd05b3da899a3399a7d0100
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2022-11-30 15:23:45 +0100

    nice

--- delete.php
+++ delete.php
@@ -9,8 +9,43 @@
 <body>
 
 <?php
+try {
+    $db = new PDO("mysql:host=localhost;dbname=cijfersysteem"
+        , "root");
+    if (isset($_POST['update'])) {
+        $leerling = filter_input(INPUT_POST, "leerling", FILTER_SANITIZE_STRING);
+        $vak = filter_input(INPUT_POST, "vak", FILTER_SANITIZE_STRING);
+        $cijfer = filter_input(INPUT_POST, "cijfer", FILTER_SANITIZE_FLOAT);
 
+        $query = $db->prepare("UPDATE resultaten SET vak = :vak, cijfer = :cijfer WHERE id = :id");
+        $query->bindParam("leerling", $leerling);
+        $query->bindParam("vak", $vak);
+        $query->bindParam("cijfer", $cijfer);
+        if ($query->execute()) {
+            echo "<br>";
+            echo "De nieuwe gegevens zijn toegevoegd.";
+            echo "<br>";
+        } else {
+            echo "Er is een fout opgetreden.";
+        }
+    }else {
+        $query = $db->prepare("SELECT * FROM resultaten WHERE id = :id");
 
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($result as &$data) {
+            echo "<tr>";
+            echo "<td>" . $data["id"] . "</td>";
+            echo "<td>" . $data["leerling"] . "</td>";
+            echo "<td>" . $data["vak"] . "</td>";
+            echo "<td>" . $data["cijfer"] . "</td>";
+            echo "</tr>";
+        }
+    }
+}
+catch(PDOException $e) {
+    die("Error!: " . $e->getmessage());
+}
 ?>
 
 </body>
